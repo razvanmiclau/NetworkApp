a718e82e115db64cade371384ca9925e3d9667f2
razvanmiclau <razvan.miclau@gmail.com>
2014-07-30 19:26:19 +0100

    Implementing the functionality for submiting statuses

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,2 +1,9 @@
 class StatusesController < ApplicationController
+	def create
+		status = Status.new(params[:status])
+		status.user_id = current_user.id
+
+		flash[:error] = "Your Status was over 140 characters" unless status.save
+		redirect_to root_url
+	end
 end

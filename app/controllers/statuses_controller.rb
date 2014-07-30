d420da5c6c3e2c9f0256cbbb6f46c128d15895f5
razvanmiclau <razvan.miclau@gmail.com>
2014-07-30 19:30:51 +0100

    Quickly added strong parameters for the status, so that submitting a status could actually work.

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,9 +1,13 @@
 class StatusesController < ApplicationController
 	def create
-		status = Status.new(params[:status])
+		status = Status.new(status_params)
 		status.user_id = current_user.id
 
 		flash[:error] = "Your Status was over 140 characters" unless status.save
 		redirect_to root_url
+	end
+
+	def status_params
+		params.require(:status).permit(:content)
 	end
 end
